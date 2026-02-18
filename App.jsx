33f5ed085154a6911b638e35e2a9c4d66092fda3
Jada Baron <jadachantellebaron@gmail.com>
2026-02-18 18:33:58 +0000

    Create function that returns 10 random numbers
    - Currently console logging arrays for now for testing

--- App.jsx
+++ App.jsx
@@ -1,6 +1,17 @@
 import Die from "./components/Die";
 
 export default function App() {
+  function generateAllNewDice() {
+    const newDiceArr = [];
+    for (let i = 0; i < 10; i++) {
+      const randomNum = Math.ceil(Math.random() * 6);
+      newDiceArr.push(randomNum);
+    }
+    return newDiceArr;
+  }
+
+  console.log(generateAllNewDice());
+
   return (
     <main>
       <div className="dice-container">
