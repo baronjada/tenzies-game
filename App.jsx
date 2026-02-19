66ccddfbdaa9b8b9b606bd9d3dadc36e827ca8e5
Jada Baron <jadachantellebaron@gmail.com>
2026-02-19 18:30:15 +0000

    Add hold function logic
    - Mapping over and updating dice array state to return new array of dice with updated isHeld property for Die that has been clicked on

--- App.jsx
+++ App.jsx
@@ -35,7 +35,11 @@ export default function App() {
   }
 
   function hold(id) {
-    console.log(id);
+    setDice((prevDice) =>
+      prevDice.map((item) =>
+        item.id === id ? { ...item, isHeld: !item.isHeld } : item,
+      ),
+    );
   }
 
   return (
