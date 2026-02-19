3636f88352278857b5125736f3ef053d2160fea8
Jada Baron <jadachantellebaron@gmail.com>
2026-02-19 19:07:19 +0000

    Add game title and instructions

--- App.jsx
+++ App.jsx
@@ -50,6 +50,11 @@ export default function App() {
 
   return (
     <main>
+      <h1>Tenzies</h1>
+      <p>
+        Roll until all dice are the same. Click each die to freeze it at its
+        current number between rolls.
+      </p>
       <div className="dice-container">{diceElements}</div>
       <button onClick={rollDice} className="roll-dice-button">
         Roll
